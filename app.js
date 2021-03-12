324142a4f6ea14210189363b51866d3d4ddb194b
Evander97 <evander.cristiano.com.br>
2021-03-12 11:52:58 -0300

    Definido Body e tratando o CORS

--- app.js
+++ app.js
@@ -6,10 +6,27 @@ const rotaProdutos = require('./routes/produtos');
 const rotaPedidos = require('./routes/pedidos');
 
 app.use(morgan('dev'));
+app.use(express.urlencoded({extended: false})); //apenas dados simples
+app.use(express.json()); //json de entrada no body  
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header (
+        'Access-Control-Allow-Header',
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    );
+     if(req.method === 'Options') {
+         res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE');
+         return res.status(200).send({});
+     }
+
+     next();
+})
 
 app.use('/produtos', rotaProdutos);
 app.use('/pedidos', rotaPedidos);
 
+// Quando não encontra a rota, entra aqui:
 app.use((req, res, next) => {
     const erro = new Error('Não encontrado!'); 
     erro.status= 404;
--- routes/produtos.js
+++ routes/produtos.js
@@ -12,8 +12,14 @@ router.get('/', (req, res, next)=> {
 
 // INSERE UM PRODUTO
 router.post('/', (req, res, next) => {
+
+    const produto = {
+        nome: req.body.nome,
+        preco: req.body.preco
+    }
     res.status(201).send({
-        mensagem: 'Produto inserido'
+        mensagem: 'Produto inserido',
+        produtoCriado: produto
     })
 });
 
