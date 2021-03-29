fbe256aa55a4b79e736dca82583faf6d979ffa69
Evander97 <evander.cristiano.com.br>
2021-03-28 22:35:49 -0300

    Controle de Autenticação de Usuário

--- routes/produtos.js
+++ routes/produtos.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const mysql = require('../mysql').pool;
 const multer = require('multer');
+const login = require('../middleware/login');
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb){
@@ -60,7 +61,7 @@ router.get('/', (req, res, next)=> {
 
 
 // INSERE UM PRODUTO
-router.post('/', upload.single('produto_img'), (req, res, next) => {
+router.post('/', login.obrigatorio, upload.single('produto_img'), (req, res, next) => {
     console.log(req.file)
     mysql.getConnection((error, conn) => {
         
@@ -130,7 +131,7 @@ router.get('/:id_produto', (req, res, next)=> {
 })
 
 // ALTERA UM PRODUTO
-router.patch('/', (req, res, next) => {
+router.patch('/', login.obrigatorio, (req, res, next) => {
     mysql.getConnection((error, conn) => {
         
         conn.query(
@@ -163,7 +164,7 @@ router.patch('/', (req, res, next) => {
 });
 
 // DELETA UM PRODUTO
-router.delete('/', (req, res, next) => {
+router.delete('/', login.obrigatorio, (req, res, next) => {
     mysql.getConnection((error, conn) => {
         
         conn.query(
